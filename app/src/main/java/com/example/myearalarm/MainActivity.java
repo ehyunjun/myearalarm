70ec0b80ca6d5e5371224f7858275fb9a9042c24
Hyun-Jun <eguswns01@gmail.com>
2025-12-08 20:44:12 +0900

    불피요한 xml 삭제 / +버튼 동작 추가

--- app/src/main/java/com/example/myearalarm/MainActivity.java
+++ app/src/main/java/com/example/myearalarm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myearalarm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnAddClockAlarm;
+    private ImageButton btnTimerAlarm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnAddClockAlarm = findViewById(R.id.btnAddClockAlarm);
+        btnTimerAlarm = findViewById(R.id.btnTimerAlarm);
+
+        btnAddClockAlarm.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddClockAlarmActivity.class);
+            startActivity(intent);
+        });
+
+        btnTimerAlarm.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddTimerAlarmActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
